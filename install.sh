1666fb601122cf8cb1d1405068072960097c4ac5
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-03-03 13:39:24 -0500

    Simplify installation, improve examples and README (#3)
    * Simplify install.sh script
    
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>
    
    * Add/update examples, update README
    
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>
    
    * Update README, add link to competition web-page
    
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>
    
    * remove unused block, format dummy_method.py
    
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>
    
    * Fix formatting typo in example_method.py
    
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>
    
    * Remove Windows hate from README
    
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-set -e
-EVALAI_TOKEN=${1?"Usage: $0 <EVALAI_TOKEN>"}
-
 
 echo "=== CREATING CONDA ENV 'clcomp21'"
-#source `conda info --base`/bin/activate base
-conda env create -f environment.yaml || true
+# source `conda info --base`/bin/activate base
+conda env create -f environment.yaml
+# echo "`conda shell.bash hook`"
 conda activate clcomp21
 
 echo "==== Installing Python packages (this may take a while) "
@@ -13,6 +11,15 @@ pip install -r requirements.txt
 # NOTE: Required step, as evalai causes a dependency conflict with `Sequoia`.
 pip install --upgrade wandb
 
-evalai set_token "$EVALAI_TOKEN"
+
+EVALAI_TOKEN=`cat evalai_token.txt`
+if [ $EVALAI_TOKEN == "YOUR_TOKEN_HERE" ]; then
+    echo "Skipping evalai sign-in. "
+    echo "NOTE: You need to copy your EvalAI Token from https://eval.ai/web/profile and paste it inside evalai_token.txt file."
+    exit 1
+else
+    echo "Signing in to evalai using provided token."
+    evalai set_token "$EVALAI_TOKEN"
+fi
 
 echo "=== DONE. Now run 'make sl' for running the sl track or 'make rl' for the RL track or 'make help'"
